43d551b0d7755eb9a518f57dab9c06cf52f3d737
Jacob Glueck <swimgiraffe435@gmail.com>
2023-05-31 20:49:03 -0700

    Update nix version

--- flake.nix
+++ flake.nix
@@ -1,8 +1,7 @@
 {
   description = "Bobcat keyboard layout";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.qmk-compile-nix = {
     # url = "git+file:/home/jacob/Documents/MyStuff/projects/qmk-compile-nix";
@@ -10,32 +9,38 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-unstable, flake-utils, qmk-compile-nix }:
+  outputs = { self, nixpkgs, flake-utils, qmk-compile-nix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         qmk = pkgs.fetchFromGitHub {
           owner = "zsa";
           repo = "qmk_firmware";
+          # firmware22 branch
+          # rev = "cf87d88fb228d9118480a98c909508adacb64f26";
+          # sha256 = "pHlne7ZcXBI5ILIlnDH2JdUZMd6TlehaNwHz/cpE6x0=";
+
           rev = "ec2a7452fcbe47078a20cb095fb8a77e26cdc6f5";
           sha256 = "WGABahZ/i+vMNclYe7AXRmP5D7vEoDQdgPBRVK7KwF0=";
+
           fetchSubmodules = true;
         };
-        unstable-pkgs = nixpkgs-unstable.legacyPackages.${system};
-        wally = unstable-pkgs.wally-cli;
+        wally = pkgs.wally-cli;
       in rec {
-        defaultPackage = qmk-compile-nix.lib.mkKeyboardFirmware {
-          inherit qmk system;
-          keyboard = "ergodox_ez";
-          firmware = "${self}";
-          name = "bobcat";
+        packages = {
+          default = qmk-compile-nix.lib.mkKeyboardFirmware {
+            inherit qmk system;
+            keyboard = "ergodox_ez";
+            firmware = "${self}";
+            name = "bobcat";
+          };
+          wally-cli = wally;
         };
-        packages.wally-cli = wally;
-        defaultApp = {
+        apps.default = {
           type = "app";
           program = let
             flasher = pkgs.writeScript "flash" ''
-              ${wally}/bin/wally-cli ${defaultPackage}
+              ${wally}/bin/wally-cli ${packages.default}
             '';
           in "${flasher}";
         };
